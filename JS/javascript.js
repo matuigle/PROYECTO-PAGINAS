2cda26960959731149fe5eccf9713f84c73ea400
matuigle <maattuuigle@gmail.com>
2024-05-26 12:25:24 -0300

    js

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,33 +1,3 @@
-const el = document.getElementsByClassName(".p3")
-const height = el.clientHeight
-const width = el.clientWidth
-
-el.addEventListener("mousemove", (evt) =>{
-    const {layerX, layerY} = evt
-    const yRotation = (
-        (layerX - width / 2) / width
-    ) * 20
-    const xRotation = (
-        (layerY - height / 2) / height
-    ) * 20
-    const string = `
-    perspective(500px) 
-    scale(1.1) 
-    rotateX(${xRotation}deg) 
-    rotateY(${yRotation}deg)`
-
-    el.style.transform = string
-})
-
-el.addEventListener('mouseout', () => {
-    el.style.transform = `
-    perspective(500px) 
-    scale(1) 
-    rotateX(0) 
-    rotateY(0)`
-})
-
-
 
 let animacion = gsap.timeline();
 animacion.from(".p3", {duration: 0.5, opacity: 0, ease: "power1.inOut" })
