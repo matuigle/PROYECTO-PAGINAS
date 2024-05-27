2db5a0686ff1eae273e5b9f3f0ec341389d3524b
MAmateoarias <162040975+MAmateoarias@users.noreply.github.com>
2024-05-26 22:25:27 -0300

    Ultimo cambio arias 26/05

--- JS/javascript.js
+++ JS/javascript.js
@@ -19,40 +19,55 @@ animacion.from(".p3", {duration: 0.5, opacity: 0, ease: "power1.inOut" })
 
 gsap.registerPlugin(MotionPathPlugin);
 
-gsap.set(".p8", {
-    x: 500, 
-    y: -500,
-});
+gsap.set(".p8", {x: 500, y: -500,});
+gsap.to(".p8", {duration: 2.5, ease: "power1.inOut",motionPath: {path: [{x: 300, y: -200}, {x: 50, y: 70}, {x: 0, y: 0},],curviness: 1.50, }})
+gsap.set(".p9", {x: -500, y: -300,});
+gsap.to(".p9", {duration: 2.5, ease: "power1.inOut",motionPath: {path: [{x: -20, y: 30}, {x: 0, y: 0},],curviness: 1.50, }});
 
-gsap.to(".p8", {
-    duration: 2.5, 
-    ease: "power1.inOut",
-    motionPath: {
-        path: [
-            {x: 300, y: -200}, 
-            {x: 50, y: 70}, 
-            {x: 0, y: 0},
-        ],
-        curviness: 1.50, 
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.fromTo(".presentacion1", 
+    {
+        opacity: 0, 
+        y: 100
+    }, 
+    {
+        opacity: 1,
+        y: 0,
+        duration: 4,
+        scrollTrigger: {
+            trigger: ".presentacion1", // Elemento que activa la animación
+            start: "top 80%", // Iniciar la animación cuando el elemento está en 80% de la vista
+            end: "top 100%", // Terminar la animación cuando el elemento está en 100% de la vista
+            scrub: true // Smooth scrubbing, animación sincronizada con el scroll
+        }
     }
-})
+);
 
-gsap.set(".p9", {
-    x: -500, 
-    y: -300,
-});
+let currentIndex = 0;
+const items = document.querySelectorAll('.carousel-item');
+const totalItems = items.length;
 
-gsap.to(".p9", {
-    duration: 2.5, 
-    ease: "power1.inOut",
-    motionPath: {
-        path: [
-            {x: -20, y: 30}, 
-
-            {x: 0, y: 0},
-        ],
-        curviness: 1.50, 
+function showSlide(index) {
+    if (index >= totalItems) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalItems - 1;
+    } else {
+        currentIndex = index;
     }
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentIndex);
 });
-
-
